46b6230487d7e8e1c006a971851bdb463fd8c569
riantechy <bchirchir97@gmail.com>
2025-10-26 22:32:01 +0300

    removed change

--- frontend/app/admin-dashboard/page.tsx
+++ frontend/app/admin-dashboard/page.tsx
@@ -231,7 +231,6 @@ export default function AdminDashboard() {
                       card.value
                     )}
                   </div>
-                  <small>{card.change}</small>
                 </Card.Body>
               </Card>
             </div>
